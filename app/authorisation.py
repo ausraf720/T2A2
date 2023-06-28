cedc8f75a9bc0c8a4783ea1915b5fd46494e3530
Raphael Florea <65544863+ausraf720@users.noreply.github.com>
2023-06-28 21:03:17 +1000

    Made log in functionality

--- app/authorisation.py
+++ app/authorisation.py
@@ -26,6 +26,29 @@ def auth_register():
 
 #\***************************************************************************\
 
+@auth_bp.route("/login/", methods=["POST"])
+def auth_login():
+    
+    #Obtain information from request
+    user_fields = user_schema.load(request.json)
+    user = Users.query.filter_by(username=user_fields["username"]).first()
+
+    #If username and or password doesn't match, send error
+    if not user or not bcrypt.check_password_hash(user.password, user_fields["password"]):
+        return abort(401, description="Incorrect username or password")
+    
+    #Otherwise, return...
+    else:
+        return "token"
+
+#\***************************************************************************\
+
+
+
+
+
+
+
 
 #MAY GET DELETED LATER
 #\***************************************************************************\
