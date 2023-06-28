0f4a15a2370e7958decbd66902d42902c02cd643
Raphael Florea <65544863+ausraf720@users.noreply.github.com>
2023-06-28 21:36:24 +1000

    Added in JWT functionality
    Now when user logs in, they get JWT token

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from init import db, ma, bcrypt
+from init import db, ma, bcrypt, jwt
 from controllers import db_commands
 from routes import review_bp
 from authorisation import auth_bp
@@ -21,14 +21,20 @@ USERNAME = os.environ.get('USERNAME')
 PORT_NUM = os.environ.get('PORT_NUM')
 DATABASE = os.environ.get('DATABASE')
 
+SECRET_JWT_KEY = os.environ.get('SECRET_JWT_KEY')
+
 #Set database URI
 uri = f'postgresql+psycopg2:' + \
     f'//{USERNAME}:{PASSWORD}@localhost:{PORT_NUM}/{DATABASE}'
 app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
+#Set JWT key
+app.config["JWT_SECRET_KEY"] = SECRET_JWT_KEY
+
 db.init_app(app)
 ma.init_app(app)
 bcrypt.init_app(app)
+jwt.init_app(app)
 
 #Register commands
 app.register_blueprint(db_commands)
--- app/authorisation.py
+++ app/authorisation.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request, abort
-from init import db, bcrypt
+from init import db, bcrypt, jwt
 from models import Users, user_schema, users_schema
+from datetime import timedelta
+from flask_jwt_extended import create_access_token
 
 auth_bp = Blueprint('auth', __name__, url_prefix="/auth")
 
@@ -37,17 +39,17 @@ def auth_login():
     if not user or not bcrypt.check_password_hash(user.password, user_fields["password"]):
         return abort(401, description="Incorrect username or password")
     
-    #Otherwise, return...
+    #Otherwise, generate access token for user
     else:
-        return "token"
+
+        #Allow token to last only 1 day
+        expiry = timedelta(days=1)
+        access_token = create_access_token(identity=str(user.user_id), expires_delta=expiry)
+
+        #Return the user and their new access token
+        return jsonify({"User":user.username, "Token": access_token })
 
 #\***************************************************************************\
-
-
-
-
-
-
 
 
 #MAY GET DELETED LATER
